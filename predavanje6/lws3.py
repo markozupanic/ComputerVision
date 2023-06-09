08e820f740f318906fa616ebf05124293dad0b85
Marko Zupanic <104766005+markozupanic@users.noreply.github.com>
2023-06-09 23:38:44 +0200

    rjeseno

--- predavanje6/lws3.py
+++ predavanje6/lws3.py
@@ -7,7 +7,6 @@ import cv2
 from matplotlib import pyplot as plt
 
 
-
 def plotArea(image, pts):
     for i in range(0,4):
         cv2.circle(image, (pts[i,0],pts[i,1]), radius=5, color=(255, 0, 0), thickness=-1)
@@ -71,9 +70,6 @@ def getTwoPeaks(binary_img):
 
     return xl, xr
 
-
-
-
 def displayOriginal(original_img, left_x, right_x, M):
     lane = np.array([
     [left_x, 0],
@@ -100,7 +96,102 @@ pathResults = 'results/'
 pathVideos = 'videos/'
 videoName  = 'video1.mp4'
 
-#TODO: kreirati opencv prozore, cv2.namedWindow()
+#LWS3
+def slidingWindow(mask, xl, xr, warped_image):
+    #binary image height
+    height = mask.shape[0]
+
+    #sl win dimensions
+    window_size_y = 30
+    window_size_x = 90
+
+    noWindows = height//(window_size_y*2)
+
+    #starting coords
+    y_pos = height - window_size_y
+    x_pos = xl
+    x_pos_d=xr
+    y_pos_d=height + window_size_y
+    
+    x_pos_d=xr
+    
+
+    #last coordinates
+    x_last_d=x_pos_d
+    x_last = x_pos
+    y_last = y_pos
+    
+
+    leftPtsX = np.empty((0, 1), np.int32)
+    leftPtsY = np.empty((0, 1), np.int32)
+    
+    rightPtsX=np.empty((0, 1), np.int32)
+    rightPtsY=np.empty((0, 1), np.int32)
+
+    for i in range(0, noWindows):
+        cv2.rectangle(warped_image,
+                    (x_pos-window_size_x, y_pos-window_size_y), 
+                    (x_pos+window_size_x, y_pos+window_size_y),
+                    (255, 0, 0), 3)
+
+        y, x = np.where(mask[y_pos-window_size_y:y_pos+window_size_y, 
+                             x_pos-window_size_x:x_pos+window_size_x]==255)
+        cv2.rectangle(warped_image,
+                    (x_pos_d-window_size_x, y_pos-window_size_y), 
+                    (x_pos_d+window_size_x, y_pos+window_size_y),
+                    (255, 0, 0), 3)
+
+        y, x = np.where(mask[y_pos-window_size_y:y_pos+window_size_y, 
+                             x_pos_d-window_size_x:x_pos_d+window_size_x]==255)
+        
+        if x.size > 0 and y.size > 0:
+            x_reshaped = np.reshape(x+x_pos-window_size_x, (len(x), 1))
+            leftPtsX = np.append(leftPtsX, x_reshaped, axis=0)
+            y_reshaped = np.reshape(y+y_pos-window_size_y, (len(y), 1))
+            leftPtsY = np.append(leftPtsY, y_reshaped, axis=0)
+
+            x_pos = int(np.mean(x)) + x_pos-window_size_x
+        else:
+            x_pos = x_last
+        
+        if x.size > 0 and y.size > 0:
+            x_reshaped = np.reshape(x+x_pos_d-window_size_x, (len(x), 1))
+            rightPtsX = np.append(rightPtsX, x_reshaped, axis=0)
+            y_reshaped = np.reshape(y+y_pos_d-window_size_y, (len(y), 1))
+            rightPtsY = np.append(rightPtsY, y_reshaped, axis=0)
+
+            x_pos_d = int(np.mean(x)) + x_pos_d-window_size_x
+        else:
+            x_pos_d = x_last_d
+        
+        y_pos_d=y_last + (window_size_y*2)
+        y_pos = y_last - (window_size_y*2)
+        x_last = x_pos
+        y_last = y_pos
+        x_last_d=x_pos_d
+        
+
+    if leftPtsX.shape[0] >= 3:
+        left_line = np.polyfit(leftPtsY[:, 0], leftPtsX[:, 0], 2)
+    else:
+        left_line = np.zeros((3, 1))
+
+    return left_line
+
+
+def plotPolyLine(warped_image, line):
+    y_coords = np.linspace(0, warped_image.shape[0]-1, warped_image.shape[0])
+    x_coords = line[0]*(y_coords**2) + line[1]*y_coords + line[2]
+
+    y_coords, x_coords = y_coords.astype(np.uint32), x_coords.astype(np.uint32)
+
+    try:
+        for x, y in zip(x_coords, y_coords):
+            cv2.line(warped_image, (x-4, y), (x+4, y), (0, 255, 0), 2)
+    except:
+        pass
+
+    
 
 
 #TODO: otvoriti video i dohvatiti width i height
@@ -111,10 +202,10 @@ height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
 #TODO: pronaci 4 tocke za transformaciju perspektive
 src = np.array([
-    [300,583],
-    [170,684],
-    [1190,666],
-    [1035,585]
+    [466, 510],
+    [181, 670],
+    [1334, 670],
+    [890, 510]
 ], dtype=np.float32)
 
 dst = np.array([
@@ -156,6 +247,11 @@ while(True):
 
     # TODO: Pozovite funkciju koja prikazuje detektiranu voznu traku na originalnoj slici    
     displayOriginal(frame, xl, xr, M_inv)
+
+    left_line = slidingWindow(mask, xl, xr, warped_image)
+
+    plotPolyLine(warped_image, left_line)
+
 
     # TODO: Prikazite okvir pomocu cv2.imshow(); i sve ostale medjurezultate kada ih napravite
     cv2.imshow("frame", frame)
